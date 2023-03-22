ae380ca633e797dde18661c13947003bd4158e51
Ryan Truong <ryan.truong@ufl.edu>
2023-03-22 16:54:35 -0400

    I created a new file with all the suggested and required function for sudoku.

---
+++ main.py
@@ -0,0 +1,116 @@
+def __init__(self, row_length, removed_cells):
+    jfkdljfklsdjklfjdsklfjkldsj
+
+
+def get_board(self):
+    dlkfjkldsjfkljsdklf
+
+
+def print_board(self):
+    djfkljsdklfjlkds
+
+
+def valid_in_row(self,row, num):
+    jdlkfjskldjkldjklfjsdj
+
+
+def valid_in_col(self,col, num):
+    djskfljdskljfklds
+
+
+def valid_in_box(self, row_start, col_start, num):
+    fjdkljfkldsjfkljsdjfklsj
+
+
+def is_valid(self, row, col, num):
+    jdsklfjdklsj
+
+
+def fill_box(self, row_start, col_start):
+    djklfsjdklfjlksj
+
+
+def fill_diagonal(self):
+    djsklfjdskljfkljsdj
+
+
+def fill_remaining(self, row, col):
+    djsklfjkdljsklfj
+
+
+def fill_values(self):
+    kfjsdkljfkldsjkl
+
+
+def remove_cells(self):
+    djsfkljdklsfjklds
+
+
+def generate_sudoku(size, removed):
+    sfjdklfjklsdjklfjsdklj
+
+
+# The following functions are optional for a single cell
+def __init__(self, value, row, col, screen):
+    djsklfjdkljsjk
+
+
+def set_cell_value(self, value):
+    dsjfkldjsklflkdsjkjf
+
+
+def draw(self):
+    djfkljdsklfjdjsklfjksl
+
+
+# The following functions are optional for an entire Sudoku Board. A Board object has 81 Cell objects
+def __init__(self, width, height, screen, difficulty):
+    djsklfjdkjfklsj
+
+
+def draw(self):
+    jdklfjskldjlfds
+
+
+def select(self, row, col):
+    djklfjskldfkjdsk
+
+
+def click(self, x, y):
+    djklfsjdskljfjdskljf
+
+
+def clear(self):
+    dfkljdsklklfjdskfljdf
+
+
+def sketch(self, value):
+    kfdjklfjdskfljds
+
+
+def place_number(self, value):
+    fjdklfjkldsjfdjs
+
+
+def reset_to_original(self):
+    kldsjflkdsjklfjkdsljfd
+
+
+def is_full(self):
+    fkljdsklfjdskljfkldsj
+
+
+def update_board(self):
+    fdkfjkldsjlfkjdslj
+
+
+def find_empty(self):
+    jdklfjdskljfklsdj
+
+
+def check_board(self):
+    lkdjfkljdskljfkldsjklfjdslj
+
+
+if __name__ == '__main__':
+    djksfljdklfjskdl
