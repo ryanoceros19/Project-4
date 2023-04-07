f960629fcb82fcd264f2a6191bdc561a7da360de
ryanoceros19 <ryantruongcollege@gmail.com>
2023-04-07 02:35:26 -0400

    I seperated the sudoku generator class into its own file. I also forgot to put the functions under actual classes, so I went ahead and did that. Instead of putting giberish under each function, I wrote "pass" so PyCharm doesn't have a stroke thinking I made a typo LOL

--- Sudoku.py
+++ Sudoku.py
@@ -1,116 +1,60 @@
-def __init__(self, row_length, removed_cells):
-    jfkdljfklsdjklfjdsklfjkldsj
+from sudoku_generator import SudokuGenerator
 
 
-def get_board(self):
-    dlkfjkldsjfkljsdklf
-
-
-def print_board(self):
-    djfkljsdklfjlkds
-
-
-def valid_in_row(self,row, num):
-    jdlkfjskldjkldjklfjsdj
-
-
-def valid_in_col(self,col, num):
-    djskfljdskljfklds
-
-
-def valid_in_box(self, row_start, col_start, num):
-    fjdkljfkldsjfkljsdjfklsj
-
-
-def is_valid(self, row, col, num):
-    jdsklfjdklsj
-
-
-def fill_box(self, row_start, col_start):
-    djklfsjdklfjlksj
-
-
-def fill_diagonal(self):
-    djsklfjdskljfkljsdj
-
-
-def fill_remaining(self, row, col):
-    djsklfjkdljsklfj
-
-
-def fill_values(self):
-    kfjsdkljfkldsjkl
-
-
-def remove_cells(self):
-    djsfkljdklsfjklds
-
-
-def generate_sudoku(size, removed):
-    sfjdklfjklsdjklfjsdklj
+def generate_sudoku(size, removed):     # This function is NOT in the SudokuGenerator class
+    pass
 
 
 # The following functions are optional for a single cell
-def __init__(self, value, row, col, screen):
-    djsklfjdkljsjk
+class Cell:
+    def __init__(self, value, row, col, screen):
+        pass
 
+    def set_cell_value(self, value):
+        pass
 
-def set_cell_value(self, value):
-    dsjfkldjsklflkdsjkjf
-
-
-def draw(self):
-    djfkljdsklfjdjsklfjksl
+    def draw(self):
+        pass
 
 
 # The following functions are optional for an entire Sudoku Board. A Board object has 81 Cell objects
-def __init__(self, width, height, screen, difficulty):
-    djsklfjdkjfklsj
+class Board:
+    def __init__(self, width, height, screen, difficulty):
+        pass
 
+    def draw(self):
+        pass
 
-def draw(self):
-    jdklfjskldjlfds
+    def select(self, row, col):
+        pass
 
+    def click(self, x, y):
+        pass
 
-def select(self, row, col):
-    djklfjskldfkjdsk
+    def clear(self):
+        pass
 
+    def sketch(self, value):
+        pass
 
-def click(self, x, y):
-    djklfsjdskljfjdskljf
+    def place_number(self, value):
+        pass
 
+    def reset_to_original(self):
+        pass
 
-def clear(self):
-    dfkljdsklklfjdskfljdf
+    def is_full(self):
+        pass
 
+    def update_board(self):
+        pass
 
-def sketch(self, value):
-    kfdjklfjdskfljds
+    def find_empty(self):
+        pass
 
-
-def place_number(self, value):
-    fjdklfjkldsjfdjs
-
-
-def reset_to_original(self):
-    kldsjflkdsjklfjkdsljfd
-
-
-def is_full(self):
-    fkljdsklfjdskljfkldsj
-
-
-def update_board(self):
-    fdkfjkldsjlfkjdslj
-
-
-def find_empty(self):
-    jdklfjdskljfklsdj
-
-
-def check_board(self):
-    lkdjfkljdskljfkldsjklfjdslj
+    def check_board(self):
+        pass
 
 
 if __name__ == '__main__':
-    djksfljdklfjskdl
+    pass
---
+++ sudoku_generator.py
@@ -0,0 +1,37 @@
+class SudokuGenerator:
+    def __init__(self, row_length, removed_cells):
+        pass
+
+    def get_board(self):
+        pass
+
+    def print_board(self):
+        pass
+
+    def valid_in_row(self, row, num):
+        pass
+
+    def valid_in_col(self, col, num):
+        pass
+
+    def valid_in_box(self, row_start, col_start, num):
+        pass
+
+    def is_valid(self, row, col, num):
+        pass
+
+    def fill_box(self, row_start, col_start):
+        pass
+
+    def fill_diagonal(self):
+        pass
+
+    def fill_remaining(self, row, col):
+        pass
+
+    def fill_values(self):
+        pass
+
+    def remove_cells(self):
+        pass
+
