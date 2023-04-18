56818850f6ef3ed22910c48d3ee01a3523d33426
ryanoceros19 <ryantruongcollege@gmail.com>
2023-04-17 23:12:59 -0400

    created attributes for Board class. Essentially, the driver funciton will create a board in the board class that also has the attributes and methods from the sudoku_generator file. Ignore the actual driver function, the "if __name__ == '__main__'" part cuz it's wrong LOL

--- Sudoku.py
+++ Sudoku.py
@@ -1,4 +1,6 @@
+import sudoku_generator
 from sudoku_generator import SudokuGenerator
+
 
 # The following functions are optional for a single cell
 class Cell:
@@ -15,7 +17,16 @@ class Cell:
 # The following functions are optional for an entire Sudoku Board. A Board object has 81 Cell objects
 class Board:
     def __init__(self, width, height, screen, difficulty):
-        pass
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.difficulty = difficulty
+        if self.difficulty == "easy":
+            self.board = sudoku_generator.generate_sudoku(9, 30)
+        elif self.difficulty == "medium":
+            self.board = sudoku_generator.generate_sudoku(9, 40)
+        elif self.difficulty == "hard":
+            self.board = sudoku_generator.generate_sudoku(9, 50)
 
     def draw(self):
         pass
@@ -55,11 +66,10 @@ if __name__ == '__main__':
     game_mode = ""
     game_mode = "easy"
     if game_mode == "easy":
-        sudoku = generate_sudoku(9, 30)
-        sudoku.get_board()
+        sudoku = Board(9, 9, None, "easy")
 
     elif game_mode == "medium":
-        sudoku = generate_sudoku(9, 40)
+        sudoku = Board(9, 9, None, "medium")
 
     elif game_mode == "hard":
-        sudoku = generate_sudoku(9, 50)
+        sudoku = Board(9, 9, None, "hard")
